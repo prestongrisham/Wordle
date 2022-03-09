b46c90e3b7a17946332746a906bea8a8f3894cca
prestongrisham <prestongrisham@gmail.com>
2022-03-09 07:43:33 -0500

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Preston Grisham on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
