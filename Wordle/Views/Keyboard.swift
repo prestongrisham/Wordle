a7bb2f844cbdce73dd69f1506042b5e9032b5e13
prestongrisham <prestongrisham@gmail.com>
2022-03-09 13:45:28 -0500

    Lesson 3

---
+++ Wordle/Views/Keyboard.swift
@@ -0,0 +1,84 @@
+//
+//  Keyboard.swift
+//  Wordle
+//
+//  Created by Preston Grisham on 3/9/22.
+//
+
+import SwiftUI
+
+struct Keyboard: View {
+    
+    @EnvironmentObject var dm: WordleDataModel
+    var topRowArray = "QWERTYUIOP".map { String($0) }
+    var secondRowArray = "ASDFGHJKL".map { String($0) }
+    var thirdRowArray = "ZXCVBNM".map { String($0) }
+    
+    var body: some View {
+        VStack {
+            HStack(spacing: 2) {
+                ForEach(topRowArray, id: \.self) { letter in
+                    LetterButtonView(letter: letter)
+                }
+            }
+            HStack(spacing: 2) {
+                ForEach(secondRowArray, id: \.self) { letter in
+                    LetterButtonView(letter: letter)
+                }
+            }
+            HStack(spacing: 2) {
+                
+                EnterButton()
+                
+                ForEach(thirdRowArray, id: \.self) { letter in
+                    LetterButtonView(letter: letter)
+                }
+                
+                DeleteButton()
+            }
+        }
+    }
+}
+
+struct Keyboard_Previews: PreviewProvider {
+    static var previews: some View {
+        Keyboard()
+            .environmentObject(WordleDataModel())
+            .scaleEffect(Global.keyboardScale)
+    }
+}
+
+
+// MARK: - Special Buttons Views
+
+struct EnterButton: View {
+    @EnvironmentObject var dm: WordleDataModel
+    var body: some View {
+        Button {
+            dm.enterWord()
+        } label: {
+            Text("Enter")
+                .font(.system(size: 20))
+                .frame(width: 60, height: 50)
+                .foregroundColor(.primary)
+                .background(Color.unused)
+        }
+        .buttonStyle(.plain)
+    }
+}
+
+struct DeleteButton: View {
+    @EnvironmentObject var dm: WordleDataModel
+    var body: some View {
+        Button {
+            dm.removeLetterFromCurrentWord()
+        } label: {
+            Image(systemName: "delete.backward.fill")
+                .font(.system(size: 20))
+                .frame(width: 40, height: 50)
+                .foregroundColor(.primary)
+                .background(Color.unused)
+        }
+        .buttonStyle(.plain)
+    }
+}
---
+++ Wordle/Views/LetterButtonView.swift
@@ -0,0 +1,35 @@
+//
+//  LetterButtonView.swift
+//  Wordle
+//
+//  Created by Preston Grisham on 3/9/22.
+//
+
+import SwiftUI
+
+struct LetterButtonView: View {
+    
+    @EnvironmentObject var dm: WordleDataModel
+    var letter: String
+    
+    var body: some View {
+        Button {
+            dm.addToCurrentWord(letter)
+        } label: {
+            Text(letter)
+                .font(.system(size: 20))
+                .frame(width: 35, height: 50)
+                .background(dm.keyColors[letter])
+                .foregroundColor(.primary)
+        }
+        .buttonStyle(.plain)
+    }
+}
+
+struct LetterButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        LetterButtonView(letter: "L")
+            .environmentObject(WordleDataModel())
+        
+    }
+}
